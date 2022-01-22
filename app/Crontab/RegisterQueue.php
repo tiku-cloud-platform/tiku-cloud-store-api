0f2e3a42d8c73d4dbda52358b1407b9f521f2b60
bruceqiq <18228937997@163.com>
2022-01-22 17:03:15 +0800

    update

--- app/Crontab/RegisterQueue.php
+++ app/Crontab/RegisterQueue.php
@@ -5,10 +5,10 @@ namespace App\Crontab;
 
 use App\Mapping\RedisClient;
 use App\Mapping\UUID;
-use App\Model\Common\StoreUserScoreCollection;
 use App\Model\Shell\StorePlatformScore;
 use App\Model\Shell\StorePlatformUser;
 use App\Model\Shell\StorePlatformUserGroup;
+use App\Model\Shell\StoreUserScoreCollection;
 use App\Repository\User\User\ScoreHistoryRepository;
 use Hyperf\Crontab\Annotation\Crontab;
 use Hyperf\Di\Annotation\Inject;
@@ -30,7 +30,6 @@ class RegisterQueue
     public function execute()
     {
         $registerUser = (new RedisClient())->redisClient->rPop("register_queue");
-        var_dump("获取Redis数据", $registerUser);
         if ($registerUser) {
             try {
                 $registerUser = json_decode($registerUser, true);
@@ -38,7 +37,6 @@ class RegisterQueue
                 $scoreConfig = (new StorePlatformScore())->getScoreConfig((array)[["key", "=", "wechat_register"], ["store_uuid", "=", $registerUser["store_uuid"]]]);
                 // 查询对应的商户平台是否存在用户分组信息
                 $groupConfig = (new StorePlatformUserGroup())->getGroup((array)[["store_uuid", "=", $registerUser["store_uuid"]]]);
-                var_dump("积分配置", $scoreConfig);
                 if (!empty($scoreConfig)) {
                     $requestParams['title']       = $scoreConfig['title'];
                     $requestParams['score_key']   = $scoreConfig['key'];
@@ -49,31 +47,35 @@ class RegisterQueue
                     $requestParams['uuid']        = UUID::getUUID();
                     $requestParams['store_uuid']  = $registerUser["store_uuid"];
                     $requestParams['user_uuid']   = $registerUser["user_uuid"];
-                    var_dump("插入的数据是", $requestParams);
                     if (!$this->scoreHistoryRepository->repositoryCreate((array)$requestParams)) {
                         // 创建成功之后，重新将注册用户的信息放回Redis队列中
                         (new RedisClient())->redisClient->rPush("register_queue", json_encode($registerUser));
                     } else {
+
+
                         // 更新用户的分组信息
-                        (new StorePlatformUser())::query()->where([["uuid", "=", $registerUser["user_uuid"]]])->update(["store_platform_user_group_uuid" => $groupConfig["uuid"]]);
+                        (new StorePlatformUser())::query()->where([["uuid", "=", $registerUser["user_uuid"]]])
+                            ->update([
+                                "store_platform_user_group_uuid" => $groupConfig["uuid"]
+                            ]);
+
                         // 创建用户积分汇总信息
                         $userScoreCollectionModel = new StoreUserScoreCollection();
-                        $userScoreCollectionModel::query()->insert([
+                        $userScoreCollectionModel::query()->create([
                             "uuid"       => UUID::getUUID(),
                             "user_uuid"  => $registerUser["user_uuid"],
                             "score"      => $scoreConfig['score'],
                             "store_uuid" => $registerUser["store_uuid"],
                         ]);
                     }
-                } else {
-                    var_dump("未配置积分");
                 }
             } catch (\Throwable $throwable) {
-                var_dump($throwable->getMessage());
-                (new RedisClient())->redisClient->lPush("register_queue", json_encode($registerUser, JSON_UNESCAPED_UNICODE));
+                preg_match("/Duplicate entry/", $throwable->getMessage(), $msg);
+                var_dump($throwable->getMessage(), $throwable->getFile(), $throwable->getLine());
+                if (empty($msg)) {
+                    (new RedisClient())->redisClient->lPush("register_queue", json_encode($registerUser, JSON_UNESCAPED_UNICODE));
+                }
             }
-        } else {
-            var_dump("数据获取失败");
         }
     }
 }
--- app/Model/Shell/StoreUserScoreCollection.php
+++ app/Model/Shell/StoreUserScoreCollection.php
@@ -1,17 +1,22 @@
 <?php
 declare(strict_types = 1);
 
-namespace App\Model\Common;
+namespace App\Model\Shell;
 
-use App\Model\BaseModel;
+use App\Model\Model;
+use Hyperf\Database\Model\SoftDeletes;
 
 /**
  * 用户积分汇总
  * Class StoreUserScoreCollection
  * @package App\Model\Common
  */
-class StoreUserScoreCollection extends BaseModel
+class StoreUserScoreCollection extends Model
 {
+    use SoftDeletes;
+
+    protected $table = "store_user_score_collection";
+
     protected $fillable = [
         "uuid",
         "user_uuid",
