ae4ef9e931acc42fc0062a4368fa877b1b1499b3
bruceqiq <18228937997@163.com>
2022-01-29 10:09:26 +0800

    fix

--- app/Request/Store/Exam/CategoryValidate.php
+++ app/Request/Store/Exam/CategoryValidate.php
@@ -23,7 +23,7 @@ class CategoryValidate extends FormRequest
     public function rules(): array
     {
         return [
-            'title'         => 'required|max:10',
+            'title'         => 'required|max:32',
             'parent_uuid'   => 'sometimes|uuid|exists:store_exam_category,uuid',
             'is_show'       => ['required', Rule::in([1, 2])],
             'is_recommend'  => ['required', Rule::in([1, 2])],
@@ -37,7 +37,7 @@ class CategoryValidate extends FormRequest
     {
         return [
             'title.required'        => '名称不能为空',
-            'title.max'             => '名称不能超过10个字符',
+            'title.max'             => '名称不能超过32个字符',
             'parent_uuid.uuid'      => '上级分类格式不正确',
             'parent_uuid.exists'    => '上级分类不存在',
             'is_show.required'      => '显示状态不能为空',
