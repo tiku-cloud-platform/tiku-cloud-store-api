833bfe496fdaf1b2706127fd485f28da16676d45
7small7 <1005349393@qq.com>
2022-11-06 21:28:27 +0800

    添加试卷链接地址

--- app/Model/Common/StoreExamCollection.php
+++ app/Model/Common/StoreExamCollection.php
@@ -16,143 +16,149 @@ use Hyperf\DbConnection\Db;
  */
 class StoreExamCollection extends BaseModel
 {
-    protected $table = 'store_exam_collection';
+	protected $table = 'store_exam_collection';
 
-    protected $fillable = [
-        'uuid',
-        'title',
-        'is_show',
-        'file_uuid',
-        'orders',
-        'store_uuid',
-        'is_recommend',
-        'submit_number',
-        'exam_category_uuid',
-        'exam_time',
-        'content',
-        'level',
-        'author',
-        'audit_author',
-        "max_option_total",
-        "max_judge_total",
-        "max_reading_total",
-    ];
+	protected $fillable = [
+		'uuid',
+		'title',
+		'is_show',
+		'file_uuid',
+		'orders',
+		'store_uuid',
+		'is_recommend',
+		'submit_number',
+		'exam_category_uuid',
+		'exam_time',
+		'content',
+		'level',
+		'author',
+		'audit_author',
+		"max_option_total",
+		"max_judge_total",
+		"max_reading_total",
+		"resource_url",
+	];
 
-    protected $appends = [
-        'use_time',// 试卷答题时间(s)
-        'use_time_minutes', // 试卷答题时间(m)
-        'option_sum', // 单选试题总数
-        'reading_sum', // 简答试题总数
-        "jude_sum", // 判断试题总题数
-        "option_score", // 单选试题总分
-        "jude_score", // 判断试题总分
-        "reading_score", // 阅读理解总分
-    ];
+	protected $appends = [
+		'use_time',// 试卷答题时间(s)
+		'use_time_minutes', // 试卷答题时间(m)
+		'option_sum', // 单选试题总数
+		'reading_sum', // 简答试题总数
+		"jude_sum", // 判断试题总题数
+		"option_score", // 单选试题总分
+		"jude_score", // 判断试题总分
+		"reading_score", // 阅读理解总分
+	];
 
-    public function coverFileInfo(): BelongsTo
-    {
-        return $this->belongsTo(StorePlatformFile::class, 'file_uuid', 'uuid');
-    }
+	public function coverFileInfo(): BelongsTo
+	{
+		return $this->belongsTo(StorePlatformFile::class, 'file_uuid', 'uuid');
+	}
 
-    public function collectionType(): BelongsTo
-    {
-        return $this->belongsTo(StoreExamCategory::class, 'exam_category_uuid', 'uuid');
-    }
+	public function collectionType(): BelongsTo
+	{
+		return $this->belongsTo(StoreExamCategory::class, 'exam_category_uuid', 'uuid');
+	}
 
-    public function examCategoryInfo(): BelongsTo
-    {
-        return $this->belongsTo(StoreExamCategory::class, 'exam_category_uuid', 'uuid');
-    }
+	public function examCategoryInfo(): BelongsTo
+	{
+		return $this->belongsTo(StoreExamCategory::class, 'exam_category_uuid', 'uuid');
+	}
 
-    public function getAuditAuthorAttribute($key): string
-    {
-        return empty($key) ? '' : $key;
-    }
+	public function getAuditAuthorAttribute($key): string
+	{
+		return empty($key) ? '' : $key;
+	}
 
-    public function getAuthorAttribute($key): string
-    {
-        return empty($key) ? '' : $key;
-    }
+	public function getAuthorAttribute($key): string
+	{
+		return empty($key) ? '' : $key;
+	}
 
-    public function getContentAttribute($key): string
-    {
-        return empty($key) ? '' : $key;
-    }
+	public function getContentAttribute($key): string
+	{
+		return empty($key) ? '' : $key;
+	}
 
-    public function getUseTimeAttribute()
-    {
-        if (!empty($this->attributes['exam_time'])) {
-            $examTime = explode(':', $this->attributes['exam_time']);
-            return $examTime[0] * 3600 + $examTime[1] * 60 + ($examTime[2] > 0 ? 1 : 0);
-        }
+	public function getResourceUrlAttribute($key): string
+	{
+		return empty($key) ? '' : $key;
+	}
 
-        return 0;
-    }
+	public function getUseTimeAttribute()
+	{
+		if (!empty($this->attributes['exam_time'])) {
+			$examTime = explode(':', $this->attributes['exam_time']);
+			return $examTime[0] * 3600 + $examTime[1] * 60 + ($examTime[2] > 0 ? 1 : 0);
+		}
 
-    public function getUseTimeMinutesAttribute()
-    {
-        if (!empty($this->attributes['exam_time'])) {
-            $examTime = explode(':', $this->attributes['exam_time']);
-            return $examTime[0] * 60 + $examTime[1] + ($examTime[2] > 0 ? 1 : 0);
-        }
+		return 0;
+	}
 
-        return 0;
-    }
+	public function getUseTimeMinutesAttribute()
+	{
+		if (!empty($this->attributes['exam_time'])) {
+			$examTime = explode(':', $this->attributes['exam_time']);
+			return $examTime[0] * 60 + $examTime[1] + ($examTime[2] > 0 ? 1 : 0);
+		}
 
-    public function getOptionSumAttribute(): int
-    {
-        return (new StoreExamCollectionRelation())::query()
-            ->where('exam_collection_uuid', '=', $this->attributes['uuid'])
-            ->count(['id']);
-    }
+		return 0;
+	}
 
-    public function getReadingSumAttribute(): int
-    {
-        return (new StoreExamReadingCollectionRelation())::query()
-            ->where('collection_uuid', '=', $this->attributes['uuid'])
-            ->count(['id']);
-    }
+	public function getOptionSumAttribute(): int
+	{
+		return (new StoreExamCollectionRelation())::query()
+			->where('exam_collection_uuid', '=', $this->attributes['uuid'])
+			->count(['id']);
+	}
 
-    public function getJudeSumAttribute(): int
-    {
-        return (new StoreExamJudeCollectionRelation())::query()
-            ->where('collection_uuid', '=', $this->attributes['uuid'])
-            ->count(['id']);
-    }
+	public function getReadingSumAttribute(): int
+	{
+		return (new StoreExamReadingCollectionRelation())::query()
+			->where('collection_uuid', '=', $this->attributes['uuid'])
+			->count(['id']);
+	}
 
-    public function getOptionScoreAttribute()
-    {
-       $score =  Db::table('store_exam_collection_relation')
-            ->join('store_exam_option', 'store_exam_collection_relation.exam_uuid', '=', 'store_exam_option.uuid')
-            ->where('store_exam_collection_relation.exam_collection_uuid', '=', $this->attributes['uuid'])
-            ->whereNull('store_exam_collection_relation.deleted_at')
-            ->whereNull('store_exam_option.deleted_at')
-            ->sum('answer_income_score');
+	public function getJudeSumAttribute(): int
+	{
+		return (new StoreExamJudeCollectionRelation())::query()
+			->where('collection_uuid', '=', $this->attributes['uuid'])
+			->count(['id']);
+	}
 
-       return empty($score) ? 0.00 : $score;
-    }
+	public function getOptionScoreAttribute()
+	{
+		$score = Db::table('store_exam_collection_relation')
+			->join('store_exam_option', 'store_exam_collection_relation.exam_uuid', '=', 'store_exam_option.uuid')
+			->where('store_exam_collection_relation.exam_collection_uuid', '=', $this->attributes['uuid'])
+			->whereNull('store_exam_collection_relation.deleted_at')
+			->whereNull('store_exam_option.deleted_at')
+			->sum('answer_income_score');
 
-    public function getReadingScoreAttribute()
-    {
-        $score = Db::table('store_exam_reading_collection_relation')
-            ->join('store_exam_reading', 'store_exam_reading_collection_relation.exam_uuid', '=', 'store_exam_reading.uuid')
-            ->where('store_exam_reading_collection_relation.collection_uuid', '=', $this->attributes['uuid'])
-            ->whereNull('store_exam_reading_collection_relation.deleted_at')
-            ->whereNull('store_exam_reading.deleted_at')
-            ->sum('answer_income_score');
+		return empty($score) ? 0.00 : $score;
+	}
 
-        return empty($score) ? 0.00 : $score;
-    }
+	public function getReadingScoreAttribute()
+	{
+		$score = Db::table('store_exam_reading_collection_relation')
+			->join('store_exam_reading', 'store_exam_reading_collection_relation.exam_uuid', '=', 'store_exam_reading.uuid')
+			->where('store_exam_reading_collection_relation.collection_uuid', '=', $this->attributes['uuid'])
+			->whereNull('store_exam_reading_collection_relation.deleted_at')
+			->whereNull('store_exam_reading.deleted_at')
+			->sum('answer_income_score');
 
-    public function getJudeScoreAttribute()
-    {
-        $score = Db::table('store_exam_jude_collection_relation')
-            ->join('store_exam_judge_option', 'store_exam_jude_collection_relation.exam_uuid', '=', 'store_exam_judge_option.uuid')
-            ->where('store_exam_jude_collection_relation.collection_uuid', '=', $this->attributes['uuid'])
-            ->whereNull('store_exam_jude_collection_relation.deleted_at')
-            ->whereNull('store_exam_judge_option.deleted_at')
-            ->sum('answer_income_score');
+		return empty($score) ? 0.00 : $score;
+	}
 
-        return empty($score) ? 0.00 : $score;
-    }
+	public function getJudeScoreAttribute()
+	{
+		$score = Db::table('store_exam_jude_collection_relation')
+			->join('store_exam_judge_option', 'store_exam_jude_collection_relation.exam_uuid', '=', 'store_exam_judge_option.uuid')
+			->where('store_exam_jude_collection_relation.collection_uuid', '=', $this->attributes['uuid'])
+			->whereNull('store_exam_jude_collection_relation.deleted_at')
+			->whereNull('store_exam_judge_option.deleted_at')
+			->sum('answer_income_score');
+
+		return empty($score) ? 0.00 : $score;
+	}
 }
--- app/Service/Store/Exam/CollectionService.php
+++ app/Service/Store/Exam/CollectionService.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Service\Store\Exam;
 
@@ -19,157 +19,157 @@ use Hyperf\Di\Annotation\Inject;
  */
 class CollectionService implements StoreServiceInterface
 {
-    /**
-     * @Inject()
-     * @var CollectionRepository
-     */
-    protected $collectionRepository;
+	/**
+	 * @Inject()
+	 * @var CollectionRepository
+	 */
+	protected $collectionRepository;
 
-    public function __construct()
-    {
-    }
+	public function __construct()
+	{
+	}
 
-    /**
-     * 格式化查询条件
-     *
-     * @param array $requestParams 请求参数
-     * @return \Closure 组装的查询条件
-     */
-    public static function searchWhere(array $requestParams)
-    {
-        return function ($query) use ($requestParams) {
-            extract($requestParams);
-            if (!empty($uuid)) {
-                $query->where('uuid', '=', $uuid);
-            }
-            if (!empty($is_recommend)) {
-                $query->where('is_recommend', '=', $is_recommend);
-            }
-            if (!empty($is_show)) {
-                $query->where('is_show', '=', $is_show);
-            }
-            if (!empty($exam_category_uuid)) {
-                $query->where('exam_category_uuid', '=', $exam_category_uuid);
-            }
-            if (!empty($title)) {
-                $query->where('title', 'like', '%' . $title . '%');
-            }
-        };
-    }
+	/**
+	 * 格式化查询条件
+	 *
+	 * @param array $requestParams 请求参数
+	 * @return \Closure 组装的查询条件
+	 */
+	public static function searchWhere(array $requestParams)
+	{
+		return function ($query) use ($requestParams) {
+			extract($requestParams);
+			if (!empty($uuid)) {
+				$query->where('uuid', '=', $uuid);
+			}
+			if (!empty($is_recommend)) {
+				$query->where('is_recommend', '=', $is_recommend);
+			}
+			if (!empty($is_show)) {
+				$query->where('is_show', '=', $is_show);
+			}
+			if (!empty($exam_category_uuid)) {
+				$query->where('exam_category_uuid', '=', $exam_category_uuid);
+			}
+			if (!empty($title)) {
+				$query->where('title', 'like', '%' . $title . '%');
+			}
+		};
+	}
 
-    /**
-     * 查询数据
-     *
-     * @param array $requestParams 请求参数
-     * @return array 查询结果
-     */
-    public function serviceSelect(array $requestParams): array
-    {
-        return $this->collectionRepository->repositorySelect(self::searchWhere((array)$requestParams),
-            (int)$requestParams['size'] ?? 20);
+	/**
+	 * 查询数据
+	 *
+	 * @param array $requestParams 请求参数
+	 * @return array 查询结果
+	 */
+	public function serviceSelect(array $requestParams): array
+	{
+		return $this->collectionRepository->repositorySelect(self::searchWhere((array)$requestParams),
+			(int)$requestParams['size'] ?? 20);
 
 //        $items['items'] = DataFormatter::recursionData((array)$items['items']);
-    }
+	}
 
-    /**
-     * 查询数据
-     *
-     * @param array $requestParams 请求参数
-     * @return array 查询结果
-     */
-    public function serviceRelationSelect(array $requestParams): array
-    {
-        return $this->collectionRepository->repositoryRelationSelect(self::searchWhere((array)$requestParams),
-            (int)$requestParams['size'] ?? 20);
+	/**
+	 * 查询数据
+	 *
+	 * @param array $requestParams 请求参数
+	 * @return array 查询结果
+	 */
+	public function serviceRelationSelect(array $requestParams): array
+	{
+		return $this->collectionRepository->repositoryRelationSelect(self::searchWhere((array)$requestParams),
+			(int)$requestParams['size'] ?? 20);
 
-    }
+	}
 
-    /**
-     * 创建数据
-     *
-     * @param array $requestParams 请求参数
-     * @return bool true|false
-     */
-    public function serviceCreate(array $requestParams): bool
-    {
-        $userInfo                    = UserInfo::getStoreUserInfo();
-        $requestParams['store_uuid'] = $userInfo['store_uuid'];
-        $requestParams['uuid']       = UUID::getUUID();
+	/**
+	 * 创建数据
+	 *
+	 * @param array $requestParams 请求参数
+	 * @return bool true|false
+	 */
+	public function serviceCreate(array $requestParams): bool
+	{
+		$userInfo                    = UserInfo::getStoreUserInfo();
+		$requestParams['store_uuid'] = $userInfo['store_uuid'];
+		$requestParams['uuid']       = UUID::getUUID();
 
-        return $this->collectionRepository->repositoryCreate((array)$requestParams);
+		return $this->collectionRepository->repositoryCreate($requestParams);
 
-    }
+	}
 
-    /**
-     * 更新数据
-     *
-     * @param array $requestParams 请求参数
-     * @return int 更新行数
-     */
-    public function serviceUpdate(array $requestParams): int
-    {
-        return $this->collectionRepository->repositoryUpdate((array)[
-            ['uuid', '=', $requestParams['uuid']]
-        ], (array)$requestParams);
-    }
+	/**
+	 * 更新数据
+	 *
+	 * @param array $requestParams 请求参数
+	 * @return int 更新行数
+	 */
+	public function serviceUpdate(array $requestParams): int
+	{
+		return $this->collectionRepository->repositoryUpdate([
+			['uuid', '=', $requestParams['uuid']]
+		], $requestParams);
+	}
 
-    /**
-     * 删除数据
-     *
-     * @param array $requestParams 请求参数
-     * @return int 删除行数
-     */
-    public function serviceDelete(array $requestParams): int
-    {
-        $uuidArray   = explode(',', $requestParams['uuid']);
-        $deleteWhere = [];
-        foreach ($uuidArray as $value) {
-            array_push($deleteWhere, $value);
-        }
+	/**
+	 * 删除数据
+	 *
+	 * @param array $requestParams 请求参数
+	 * @return int 删除行数
+	 */
+	public function serviceDelete(array $requestParams): int
+	{
+		$uuidArray   = explode(',', $requestParams['uuid']);
+		$deleteWhere = [];
+		foreach ($uuidArray as $value) {
+			array_push($deleteWhere, $value);
+		}
 
-        return $this->collectionRepository->repositoryWhereInDelete((array)$deleteWhere, (string)'uuid');
-    }
+		return $this->collectionRepository->repositoryWhereInDelete($deleteWhere, 'uuid');
+	}
 
-    /**
-     * 查询单条数据
-     *
-     * @param array $requestParams 请求参数
-     * @return array 删除行数
-     */
-    public function serviceFind(array $requestParams): array
-    {
-        return $this->collectionRepository->repositoryFind(self::searchWhere((array)$requestParams));
-    }
+	/**
+	 * 查询单条数据
+	 *
+	 * @param array $requestParams 请求参数
+	 * @return array 删除行数
+	 */
+	public function serviceFind(array $requestParams): array
+	{
+		return $this->collectionRepository->repositoryFind(self::searchWhere($requestParams));
+	}
 
-    /**
-     * 查询总条数
-     *
-     * @param array $requestParams
-     * @return int
-     */
-    public function serviceCount(array $requestParams = []): int
-    {
-        return $this->collectionRepository->repositoryCount(self::searchWhere((array)$requestParams));
-    }
+	/**
+	 * 查询总条数
+	 *
+	 * @param array $requestParams
+	 * @return int
+	 */
+	public function serviceCount(array $requestParams = []): int
+	{
+		return $this->collectionRepository->repositoryCount(self::searchWhere($requestParams));
+	}
 
-    /**
-     * 查询提交总数
-     *
-     * @param array $requestParams
-     * @return int
-     */
-    public function serviceSum(array $requestParams = []): int
-    {
-        return $this->collectionRepository->repositorySum(self::searchWhere((array)$requestParams), (string)'submit_number');
-    }
+	/**
+	 * 查询提交总数
+	 *
+	 * @param array $requestParams
+	 * @return int
+	 */
+	public function serviceSum(array $requestParams = []): int
+	{
+		return $this->collectionRepository->repositorySum(self::searchWhere($requestParams), 'submit_number');
+	}
 
-    /**
-     * 查询阅读理解列表
-     * @param array $requestParams
-     * @return array
-     */
-    public function serviceReadingList(array $requestParams): array
-    {
-        return (new ReadingRepository())->repositoryQuery([[""]]);
-    }
+	/**
+	 * 查询阅读理解列表
+	 * @param array $requestParams
+	 * @return array
+	 */
+	public function serviceReadingList(array $requestParams): array
+	{
+		return (new ReadingRepository())->repositoryQuery([[""]]);
+	}
 }
