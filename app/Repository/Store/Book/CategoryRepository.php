b0454ea0ecc0b1604138e041990d5b4a6da4417a
7small7 <1005349393@qq.com>
2023-01-06 22:27:54 +0800

    update

--- app/Repository/Store/Book/CategoryRepository.php
+++ app/Repository/Store/Book/CategoryRepository.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Repository\Store\Book;
 
@@ -14,98 +14,92 @@ use Hyperf\Di\Annotation\Inject;
  */
 class CategoryRepository implements StoreRepositoryInterface
 {
-	/**
-	 * @Inject()
-	 * @var StoreBookCategory
-	 */
-	protected $categoryModel;
+    public function repositorySelect(Closure $closure, int $perSize): array
+    {
+        $items = (new StoreBookCategory)::query()
+//			->with(["book:uuid,title"])
+            ->where($closure)
+            ->select([
+                "uuid",
+//				"store_book_uuid",
+                "title",
+                "parent_uuid",
+//				"is_show",
+//				"orders",
+//				"created_at",
+//				"updated_at",
+            ])
+            ->orderBy('id')
+            ->paginate($perSize);
 
-	public function repositorySelect(Closure $closure, int $perSize): array
-	{
-		$items = $this->categoryModel::query()
-			->with(["book:uuid,title"])
-			->where($closure)
-			->select([
-				"uuid",
-				"store_book_uuid",
-				"title",
-				"parent_uuid",
-				"is_show",
-				"orders",
-				"created_at",
-				"updated_at",
-			])
-			->orderBy('id')
-			->paginate($perSize);
+        return [
+            'items' => $items->items(),
+            'total' => $items->total(),
+            'size' => $items->perPage(),
+            'page' => $items->currentPage(),
+        ];
+    }
 
-		return [
-			'items' => $items->items(),
-			'total' => $items->total(),
-			'size'  => $items->perPage(),
-			'page'  => $items->currentPage(),
-		];
-	}
+    public function repositoryCreate(array $insertInfo): bool
+    {
+        if (!empty((new StoreBookCategory)::query()->create($insertInfo))) {
+            return true;
+        }
 
-	public function repositoryCreate(array $insertInfo): bool
-	{
-		if (!empty($this->categoryModel::query()->create($insertInfo))) {
-			return true;
-		}
+        return false;
+    }
 
-		return false;
-	}
+    public function repositoryAdd(array $addInfo): int
+    {
+        return 0;
+    }
 
-	public function repositoryAdd(array $addInfo): int
-	{
-		return 0;
-	}
+    public function repositoryUpdate(array $updateWhere, array $updateInfo): int
+    {
+        return (new StoreBookCategory)::query()->where($updateWhere)->update($updateInfo);
+    }
 
-	public function repositoryUpdate(array $updateWhere, array $updateInfo): int
-	{
-		return $this->categoryModel::query()->where($updateWhere)->update($updateInfo);
-	}
+    public function repositoryDelete(array $deleteWhere): int
+    {
+        return (new StoreBookCategory)::query()->where($deleteWhere)->delete();
+    }
 
-	public function repositoryDelete(array $deleteWhere): int
-	{
-		return $this->categoryModel::query()->where($deleteWhere)->delete();
-	}
+    public function repositoryWhereInDelete(array $deleteWhere, string $field): int
+    {
+        return (new StoreBookCategory)::query()->whereIn($field, $deleteWhere)->delete();
+    }
 
-	public function repositoryWhereInDelete(array $deleteWhere, string $field): int
-	{
-		return $this->categoryModel::query()->whereIn($field, $deleteWhere)->delete();
-	}
+    public function repositoryFind(Closure $closure): array
+    {
+        $bean = (new StoreBookCategory)::query()
+//            ->with(["book:uuid,title"])
+            ->select([
+                "uuid",
+//                "store_book_uuid",
+                "title",
+                "parent_uuid",
+//                "is_show",
+//                "orders",
+//                "created_at",
+//                "updated_at",
+            ])
+            ->where($closure)
+            ->first();
 
-	public function repositoryFind(\Closure $closure): array
-	{
-		$bean = $this->categoryModel::query()
-			->with(["book:uuid,title"])
-			->select([
-				"uuid",
-				"store_book_uuid",
-				"title",
-				"parent_uuid",
-				"is_show",
-				"orders",
-				"created_at",
-				"updated_at",
-			])
-			->where($closure)
-			->first();
+        if (!empty($bean)) {
+            return $bean->toArray();
+        }
+        return [];
+    }
 
-		if (!empty($bean)) {
-			return $bean->toArray();
-		}
-		return [];
-	}
-
-	public function repositorySpecial(array $searchWhere, array $fields = ["id"]): array
-	{
-		$items = $this->categoryModel::query()
-			->where($searchWhere)
-			->get($fields);
-		if (!empty($items)) {
-			return $items->toArray();
-		}
-		return [];
-	}
+    public function repositorySpecial(array $searchWhere, array $fields = ["id"]): array
+    {
+        $items = (new StoreBookCategory)::query()
+            ->where($searchWhere)
+            ->get($fields);
+        if (!empty($items)) {
+            return $items->toArray();
+        }
+        return [];
+    }
 }
