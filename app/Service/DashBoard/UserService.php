3476131702a818ad2c48d59cb80f01327279d2ed
7small7 <1005349393@qq.com>
2023-02-07 20:55:51 +0800

    增加会员新增指标数据

---
+++ app/Service/DashBoard/UserService.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Service\DashBoard;
+
+use App\Mapping\UserInfo;
+use Hyperf\DbConnection\Db;
+
+/**
+ * 会员数据指标统计
+ */
+class UserService
+{
+    /**
+     * 会员数据指标
+     * @return array
+     */
+    public function user(): array
+    {
+        /** @var string|false $currentDay 本月天数 */
+        $currentDay = date("t", strtotime(date("Y-m-d")));
+
+        /** @var int $incrementYesterday 昨日新增会员数 */
+        $incrementYesterday = Db::table("store_platform_user")->where([
+            ["store_uuid", "=", UserInfo::getStoreUserStoreUuid()]
+        ])->whereBetween("created_at", [
+            date("Y-m-d 00:00:00", strtotime("-1 day")),
+            date("Y-m-d 23:59:59", strtotime("-1 day"))
+        ])->count("id");
+
+        /** @var int $incrementCurrentMonth 本月新增会员数 */
+        $incrementCurrentMonth = Db::table("store_platform_user")->where([
+            ["store_uuid", "=", UserInfo::getStoreUserStoreUuid()]
+        ])->whereBetween("created_at", [
+            date("Y-m-01 00:00:00"),
+            date("Y-m-$currentDay 23:59:59")
+        ])->count();
+
+        return [
+            "increment" => [// 会员新增指标
+                "yesterday" => $incrementYesterday,
+                "current_month" => $incrementCurrentMonth,
+                "yesterday_circular_ratio" => 0.00,
+                "current_month_circular_ratio" => 0.00,
+            ],
+            "visitor" => [// 会员访问指标
+                "yesterday" => 0,
+                "current_month" => 0,
+                "yesterday_circular_ratio" => 0.00,
+                "current_month_circular_ratio" => 0.00,
+            ]
+        ];
+    }
+}
