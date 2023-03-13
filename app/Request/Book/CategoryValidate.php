92dea17776c0b767c8eb395186c1f3841ba1dee9
7small7 <1005349393@qq.com>
2023-03-13 21:55:30 +0800

    增加文档格式

--- app/Request/Book/CategoryValidate.php
+++ app/Request/Book/CategoryValidate.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Request\Book;
 
@@ -12,38 +12,37 @@ use Hyperf\Validation\Rule;
  */
 class CategoryValidate extends FormRequest
 {
-	public function authorize(): bool
-	{
-		return true;
-	}
+    public function authorize(): bool
+    {
+        return true;
+    }
 
-	public function rules(): array
-	{
-		return [
-			'store_book_uuid' => 'required|uuid|exists:store_book,uuid',
-			'parent_uuid'     => 'sometimes|uuid|exists:store_book_category,uuid',
-			'title'           => 'required|max:32',
-			'is_show'         => ['required', 'integer', Rule::in([2, 1])],
-			'orders'          => 'required|integer|max:100000000',
-		];
-	}
+    public function rules(): array
+    {
+        return [
+            'store_book_uuid' => 'required|uuid|exists:store_book,uuid',
+            'parent_uuid' => 'sometimes|exists:store_book_category,uuid',
+            'title' => 'required|max:32',
+            'is_show' => ['required', 'integer', Rule::in([2, 1])],
+            'orders' => 'required|integer|max:100000000',
+        ];
+    }
 
-	public function messages(): array
-	{
-		return [
-			'store_book_uuid.required' => '书籍不能为空',
-			'store_book_uuid.uuid'     => '书籍格式错误',
-			'store_book_uuid.exists'   => '书籍不存在',
-			'parent_uuid.uuid'         => '上级目录格式不正确',
-			'parent_uuid.exists'       => '上级目录不存在',
-			'title.required'           => '书籍名称不能为空',
-			'title.max'                => '书籍名称长度超过32个字符',
-			'is_show.required'         => '书籍启用状态不能为空',
-			'is_show.integer'          => '书籍启用状态格式不正确',
-			'is_show.in'               => '书籍状态值不正确',
-			'orders.required'          => '排序不能为空',
-			'orders.integer'           => '排序格式不正确',
-			'orders.max'               => '排序数字过大',
-		];
-	}
+    public function messages(): array
+    {
+        return [
+            'store_book_uuid.required' => '书籍不能为空',
+            'store_book_uuid.uuid' => '书籍格式错误',
+            'store_book_uuid.exists' => '书籍不存在',
+            'parent_uuid.exists' => '上级目录不存在',
+            'title.required' => '书籍名称不能为空',
+            'title.max' => '书籍名称长度超过32个字符',
+            'is_show.required' => '书籍启用状态不能为空',
+            'is_show.integer' => '书籍启用状态格式不正确',
+            'is_show.in' => '书籍状态值不正确',
+            'orders.required' => '排序不能为空',
+            'orders.integer' => '排序格式不正确',
+            'orders.max' => '排序数字过大',
+        ];
+    }
 }
